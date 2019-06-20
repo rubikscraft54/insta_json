1cebc042ab0e8f268c5e365dd962463a853f5612
rubikscraft54 <rubikscraft54@gmail.com>
2019-06-20 01:15:50 -0400

    Update createdf.py

--- createdf.py
+++ createdf.py
@@ -1,6 +1,7 @@
 import pandas as pd
 import os, json, time, threading, queue
 
+
 #get list of directories to work with
 def get_pathlist(dir):
     os.chdir(dir)
@@ -12,54 +13,62 @@ def get_pathlist(dir):
     pathlist = list(dict.fromkeys(pathlist))
     return pathlist
 
+
 #create a single df for a folder and export as txt
-def folder_df(q, df):
-    folder = q.get(True, 1)
-    print(threading.currentThread().getName(), 'processing', folder)
-    fileslist = []
-    for f in os.listdir(folder):
-        if f.endswith('.json'):
-            fileslist.append(os.path.join(folder, f))
-        else:
+def worker(q):
+    while True:
+        try:
+            drt = q.get_nowait()
+            folder_name = str(os.path.basename(drt))
+            df = pd.DataFrame(columns=['timestamp', 'shortcode', 'user id', 'likes', 'text'])
+            print(threading.currentThread().getName(), 'processing', folder_name)
+            fileslist = []
+            for f in os.listdir(drt):
+                if f.endswith('.json'):
+                    fileslist.append(os.path.join(drt, f))
+                else:
+                    pass
+            for i in range(len(fileslist)):
+                with open(fileslist[i]) as file:
+                    data = json.load(file)
+                    text = ''
+                    for x in data['node']['edge_media_to_caption']['edges']:
+                        text = x['node']['text']
+                    timestamp = data['node']['taken_at_timestamp']
+                    like = data['node']['edge_liked_by']['count']
+                    scode = data['node']['shortcode']
+                    id = data['node']['owner']['id']
+
+                    row = {'timestamp': timestamp, 'shortcode': scode, 'user id': id, 'likes': like, 'text': text}
+                    df = df.append(row, ignore_index=True)
+
+            print('\t', threading.currentThread().getName(), 'finished ', folder_name, 'remaining: ', q.qsize())
+            file_name = str(os.path.basename(drt) + '.txt')
+            path = r'C:/Users/Sean/Documents/Intern/txt/'
+            df.to_csv(os.path.join(path, file_name), header=True)
+            q.task_done()
+
+        except queue.Empty:
             pass
-    for i in range(len(fileslist)):
-        with open(fileslist[i]) as file:
-            data = json.load(file)
-            text = ''
-            for x in data['node']['edge_media_to_caption']['edges']:
-                text = x['node']['text']
-            timestamp = data['node']['taken_at_timestamp']
-            like = data['node']['edge_liked_by']['count']
-            scode = data['node']['shortcode']
-            id = data['node']['owner']['id']
 
-            row = {'timestamp': timestamp, 'shortcode': scode, 'user id': id, 'likes': like, 'text': text}
-            df = df.append(row, ignore_index=True)
-
-    print(threading.currentThread().getName(), 'finished processing folder ', folder)
-    fname = str(os.path.basename(folder) + '.txt')
-    path = r'C:/Users/Sean/Documents/Intern/txt/'
-    df.to_csv(os.path.join(path, fname), header=True)
-    q.task_done()
 
 def main():
-    df = pd.DataFrame(columns=['timestamp', 'shortcode', 'user id', 'likes', 'text'])
-    pl = get_pathlist('/Users/Sean/Documents/Intern/Instagram-Json')
-    wq = queue.Queue()
-    threadList = []
+    plx = get_pathlist('/Users/Sean/Documents/Intern/Instagram-Json')
+    pl = ['C:/Users/Sean/Documents/Intern/Instagram-Json/#selfharmmm',
+          'C:/Users/Sean/Documents/Intern/Instagram-Json/#selfharm',
+          'C:/Users/Sean/Documents/Intern/Instagram-Json/#cutting']
+    q = queue.Queue()
     for i in pl:
-        wq.put(i)
-    for x in range(8):
-        t = threading.Thread(target=folder_df, args=(wq, df))
-        t.setDaemon(True)
+        q.put(i)
+
+    threadList = []
+    for x in range(4):
+        t = threading.Thread(target=worker, args=(q,), daemon=True)
         threadList.append(t)
         t.start()
-        time.sleep(2)
-    wq.join()
+        time.sleep(1)
 
-    for t in threadList:
-        t.join()
-
+    q.join()
 
 
 init_time = time.time()
