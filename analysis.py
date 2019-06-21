8f1e135ab8c95f1bd5fa0070304b8d08afd1d0c8
rubikscraft54 <rubikscraft54@gmail.com>
2019-06-21 06:47:42 -0400

    Update analysis.py

--- analysis.py
+++ analysis.py
@@ -1,6 +1,7 @@
 import pandas as pd
 import os, time
 
+
 #process text data and return list of hashtags
 def hstg(str):
     raw_text = str[str.find('#'):]
@@ -26,18 +27,20 @@ def hstg(str):
 def merge(path):
     os.chdir(path)
     df = pd.DataFrame(columns=['datetime', 'shortcode', 'user id', 'likes', 'text'])
-    for f in os.listdir(path):
-        num = 88
-        csv = pd.read_csv(f)
-        for row in csv.iterrows():
-            raw_text = row['text']
+    flist = [x for x in os.listdir(path)]
+    num = len(flist)
+    for f in flist:
+        ti = time.time()
+        csv = pd.read_csv(os.path.join(path, f))
+        for i, row in csv.iterrows():
+            raw_text = str(row['text'])
             if row['shortcode'] in df.loc[:, 'shortcode'].values:
                 pass
             else:
                 row['text'] = hstg(raw_text)
                 df = df.append(row, ignore_index=True)
         num -= 1
-        print(f'Finished {os.path.basename(f)}, {num} files remaining')
+        print(f'{os.path.basename(f)} Done | {os.path.getsize(os.path.join(path, f))/1000} KB | {num} remaining | took {time.time() - ti}s')
     df['datetime'] = pd.to_datetime(df['datetime'], unit='D')
     df.sort_values(by='datetime')
     df = df.set_index(['datetime'])
@@ -86,7 +89,7 @@ def posts_byday(df):
 def hstg_count(df):
     dict = {}
     for i in df['hashtags']:
-        for h in i[h]:
+        for h in i:
             if h in dict:
                 dict[h] += 1
             else:
@@ -94,34 +97,11 @@ def hstg_count(df):
     cnt = pd.DataFrame.from_dict(dict, orient='index', columns=['count'])
     return cnt
 
-
 def main():
-    merge(r'C:\Users\Sean\Documents\Intern\txt')
-    df = opendf('C:\Users\Sean\Documents\Intern\dataframetest.txt')
-    df1819 = df_1819(df)
-
-    #export unique hashtag counts 2018-2019
-    hcnt_1819 = hstg_count(df1819)
-    export_csv = hcnt_1819.to_csv(r'C:\Users\Sean\Documents\Intern\hstgcount_1819.txt', header=True)
-
-    #export unique posts by day
-    pbd_1819 = posts_byday(df1819)
-    export_csv = pbd_1819.to_csv(r'C:\Users\Sean\Documents\Intern\posts_byday_1819.txt', header=True)
-    pbd = posts_byday(df)
-    export_csv = pbd.to_csv(r'C:\Users\Sean\Documents\Intern\posts_byday.txt', header=True)
-
-    #export unique hashtags by user
-    h_byuser = hstg_byuser(df)
-    export_csv = h_byuser.to_csv(r'C:\Users\Sean\Documents\Intern\h_byuser.txt', header=True)
-
+    main_df, main_df1819 = merge(r'F:\txt')
+    export(main_df, r'F:\analysis\MainDF_All.csv', header=True)
+    export(main_df1819, r'F:\analysis\MainDF_1819.csv', header=True)
 
 init_time = time.time()
 main()
 print("done in: ", time.time() - init_time)
-
-
-
-
-
-
-
